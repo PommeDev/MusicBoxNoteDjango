cdecc1226b8fd9cc978b4b0f63b64bbee0024d7c
PommeDev <tom.frachetdev@gmail.com>
2025-10-03 21:33:52 +0200

    Modification du chemin et des liens

--- musicbox/urls.py
+++ musicbox/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('musicnote/',include('musicnote.urls')),
+    path('',include('musicnote.urls')),
     path('admin/', admin.site.urls),
     
 ]
