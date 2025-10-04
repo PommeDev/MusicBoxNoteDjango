38baa3f990a328872c7ae20a065afd0609610f57
PommeDev <tom.frachetdev@gmail.com>
2025-10-04 22:51:15 +0200

    Modification du slider

--- musicnote/static/musicnote/sliderIndex/script.js
+++ musicnote/static/musicnote/sliderIndex/script.js
@@ -2,13 +2,13 @@ document.addEventListener('DOMContentLoaded', function () {
   // Slider 1 → gauche à droite
   new Splide('#slider-1', {
     type        : 'loop',       // boucle infinie
-    perPage     : 4,            // nombre de slides visibles
+    perPage     : 5,            // nombre de slides visibles
     autoplay    : true,         // active le défilement automatique
     interval    : 0,         // temps entre chaque transition (ms)
     pauseOnHover: false,        // continue même si on survole
     arrows      : false,        // cache les flèches
     pagination  : false,
-    speed : 50000,
+    speed : 40000,
     easing : 'linear',
     drag : false,        // pas de pagination
   }).mount();
@@ -16,13 +16,13 @@ document.addEventListener('DOMContentLoaded', function () {
 
   new Splide('#slider-2', {
     type        : 'loop',       // boucle infinie
-    perPage     : 4,            // nombre de slides visibles
+    perPage     : 5,            // nombre de slides visibles
     autoplay    : true,         // active le défilement automatique
     interval    : 0,         // temps entre chaque transition (ms)
     pauseOnHover: false,        // continue même si on survole
     arrows      : false,        // cache les flèches
     pagination  : false,
-    speed : 50000,
+    speed : 40000,
     easing : 'linear',
     drag : false,
     direction : 'rtl',        // pas de pagination
